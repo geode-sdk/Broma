257ec9e6fc6ea757c62d8071f2afc4026c20956e
Calloc <140273795+CallocGD@users.noreply.github.com>
2024-12-07 22:13:54 -0600

    Update broma.hpp

--- include/broma.hpp
+++ include/broma.hpp
@@ -9,4 +9,10 @@ namespace broma {
 	///
 	/// @param fname The path of the file you want to parse, as a string.
 	Root parse_file(std::string const& fname);
+
+	/// @brief Parses a broma file safely by not exiting when the parser throws an error
+	/// @param fname The path of the file you want to parse, as a string.
+	/// @return A root result with a boolean value to check for errors.
+	SafeRootResult parse_file_safely(std::string const& fname);
+
 }
